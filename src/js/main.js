70e93d243b5ae524b20296f79ca3c45deb01bbf3
Qin Qin <orangeqq99@gmail.com>
2022-09-24 13:56:51 -0400

    Added Scroll Aware NavBar

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,52 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//scroll aware navigation
+function selectElementByClass(className) {
+  return document.querySelector(`.${className}`);
+}
+//section class names
+const sections = [
+  selectElementByClass('home-section'),
+  selectElementByClass('skills-section'),
+  selectElementByClass('projects-section'),
+  selectElementByClass('contact-section'),
+];
+// section id : navbar a class name
+const navItems = {
+  home: selectElementByClass('homeNavItem'),
+  skills: selectElementByClass('skillsNavItem'),
+  projects: selectElementByClass('projectsNavItem'),
+  contact_me: selectElementByClass('contactNavItem'),
+};
+
+// intersection observer setup
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.7,
+};
+
+function observerCallback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // get the nav item corresponding to the id of the section
+      // that is currently in view
+      const navItem = navItems[entry.target.id];
+      // add 'active' class on the navItem
+      navItem.classList.add('active');
+      // remove 'active' class from any navItem that is not
+      // same as 'navItem' defined above
+      Object.values(navItems).forEach((item) => {
+        if (item != navItem) {
+          item.classList.remove('active');
+        }
+      });
+    }
+  });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach((sec) => observer.observe(sec));
